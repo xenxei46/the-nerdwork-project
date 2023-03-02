1ae95de0c2a140826829fd6de6497e81da816018
Wisdom John ikoi <wisdomjohnikoi@gmail.com>
2023-03-02 18:56:31 +0100

    fixing ts errors

--- nerd-work/src/App.tsx
+++ nerd-work/src/App.tsx
@@ -4,18 +4,19 @@ import EventPage from '../src/app/Layout/EventPage';
 import './App.css';
 
 interface loading {
+  loading: boolean;
   setLoadig: boolean;
 }
 
-function App() {
+function App():any {
   const [loading, setLoading] = useState(true);
   const spinner = document.getElementById('spinner');
-  // if(spinner){
-  //   setTimeout(() =>{
-  //     spinner.style.display = "none";
-  //     setLoading(false);
-  //   },2000)
-  // } 
+  if(spinner){
+    setTimeout(() =>{
+      spinner.style.display = "none";
+      setLoading(false);
+    },2000)
+  } 
   return (
     !loading && (
     <div className="w-full">
